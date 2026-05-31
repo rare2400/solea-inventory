1a38f6045abd47ad0fa2b96e4aaaa6f01beceef2
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-31 14:47:51 +0200

    import AppHeader, AppFooter to be the same on all pages and add RouterView tok make it a SPA

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-
+import AppHeader from '@/components/AppHeader.vue';
+import AppFooter from '@/components/AppFooter.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <h1>Solea jewelry lagersystem</h1>
+  <div>
+    <AppHeader />
+    <div class="d-flex">
+      <main class="container-custom flex-grow-1">
+        <RouterView />
+      </main>
+    </div>
+    <AppFooter />
+  </div>
 </template>
 
 <style scoped></style>
