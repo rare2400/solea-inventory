18636751e62e2673cb01b97adf751ac5293069a5
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-31 10:12:39 +0200

    import bootstrap to application and use router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import '@/assets/main.css'
 import App from '@/App.vue'
+import router from '@/router/router.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 // Create and mount the Vue application
 const app = createApp(App)
+app.use(router)
 app.mount('#app')
