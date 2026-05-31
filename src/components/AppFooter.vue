6046335e99c8ee8921c5d375f64001428d685276
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-31 13:58:48 +0200

    create footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+    <footer>
+        <p>Copyright &copy; 2026 Solea Jewelry</p>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    border-top: 1px solid var(--primary);
+    margin-top: 1em;
+    color: #000;
+    text-align: center;
+    padding: 1rem;
+}
+</style>
