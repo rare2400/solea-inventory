6445f2ba7c0dbe7a05557fa08aac9360bbb252cc
Ramona Reinholdz <rare2400@student.miun.se>
2026-06-01 09:03:46 +0200

    move main.css down in imports to be able to override bootstrap-styles

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import '@/assets/main.css'
 import App from '@/App.vue'
 import router from '@/router/router.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+import '@/assets/main.css'
 
 // Create and mount the Vue application
 const app = createApp(App)
