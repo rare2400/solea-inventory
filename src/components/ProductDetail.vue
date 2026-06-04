684c0caabbc954c6b9f8af499fff136567be96af
Ramona Reinholdz <rare2400@student.miun.se>
2026-06-04 12:42:13 +0200

    update button classes and add icons to ProductDetail

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -46,18 +46,26 @@
         <span class="detail-label">Lagersaldo</span>
         <div class="d-flex align-items-center gap-2 mt-1">
           <!-- Remove one from stock -->
-          <button class="btn btn-sm solea-btn-outline" 
+          <button class="btn btn-sm solea-button-outline" 
             @click="changeStock(-1)" 
             :disabled="stockLoading"
-            >-</button>
+            >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+            </svg>
+          </button>
           <span class="badge fs-6 px-3" :class="currentStock > 0 ? 'bg-success' : 'bg-danger'">
             {{ currentStock > 0 ? currentStock : "Slut i lager" }}
           </span>
           <!-- Add one to stock -->
-          <button class="btn btn-sm solea-btn-outline" 
+          <button class="btn btn-sm solea-button-outline" 
             @click="changeStock(1)"
             :disabled="stockLoading"
-            >+</button>
+            >
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+            </svg>
+          </button>
         </div>
       </div>
 
@@ -70,8 +78,11 @@
         <!-- Link to edit page -->
         <RouterLink 
           :to="`/products/${product._id}/edit`" 
-          class="btn solea-btn flex-grow-1"
+          class="btn solea-button flex-grow-1"
         >
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill mx-1" viewBox="0 0 16 16">
+              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+          </svg>
           Redigera
         </RouterLink>
 
@@ -82,6 +93,9 @@
           @click="handleDelete" 
           :disabled="deleteLoading"
         >
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+              <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+          </svg>
           {{ deleteLoading ? "Tar bort..." : "Ta bort" }}
         </button>
       </div>
